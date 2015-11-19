e97de373a8bb82b94d0ef9f3f39ca3e208ae396f
Brandon <bhanks@mit.edu>
2015-11-19 17:35:47 -0500

    moved modules to static, added the phaser tutorial game to test it, added a basic ajax script test too

---
+++ unplatform/research/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, loader
+from django.http import HttpResponse
+import socket
+# Create your views here.
+
+# This beast returns the ip address of the host machine
+def get_ip():
+    return [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+
+
+def index(request):
+    template = loader.get_template('research/index.html')
+    return HttpResponse(template.render())
