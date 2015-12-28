7a3d7fbb4fd01b6112638aa27f50a49673ad8301
Brandon <bhanks@mit.edu>
2015-12-28 18:23:17 -0500

    added a get client ip function

--- unplatform/research/models.py
+++ unplatform/research/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# class SessionUUID(models.model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     user_agent = models.CharField(max_length=150) # not sure what a good length is yet
+#     ip_address = models.CharField(max_length=15)
+#     time = models.DateTimeField(auto_now_add=True)
+#
+# class VideoData(models.Model):
+# 	uuid = models.ForeignKey(SessionUUID, related_name='uuids')
+# 	metadata = models.
+#
+# 	time = models.DateTimeField(auto_now_add=True)
+# 	def __unicode__(self):
+# 		return self.metadata
--- unplatform/research/urls.py
+++ unplatform/research/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^comprehension/$', views.comprehension, name='comprehension'),
+    url(r'^video/$', views.video, name='video')
     ]
