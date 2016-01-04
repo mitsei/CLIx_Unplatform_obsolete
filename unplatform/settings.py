3f38fc16ec2edb66b7d763f6495190655c79bb96
Brandon <bhanks@mit.edu>
2016-01-04 15:01:55 -0500

    deploying again with original engine

--- unplatform/settings.py
+++ unplatform/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'unplatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'unplatform',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
