922e8c1461c701c8f932d4096f5ed4bf2f3e498d
Brandon <bhanks@mit.edu>
2016-01-27 11:50:19 -0500

    testing if the slideshow tool is fixed by disabling cors

--- unplatform/research/urls.py
+++ unplatform/research/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^$', views.index, name='index'),
     url(r'^comprehension/$', views.comprehension, name='comprehension'),
-    url(r'^video/$', views.video, name='video')
+    url(r'^video/$', views.video, name='video'),
+    url(r'^slideshow/$', views.slideshow, name='slideshow')
     ]
