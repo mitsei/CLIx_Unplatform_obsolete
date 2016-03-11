0185ae8f65c4da96ddcd57b818f291bc4f36e78a
Brandon <bhanks@mit.edu>
2016-03-11 12:20:44 -0500

    added windows distributable, rearranged rest templates so pyinstaller picks them up

---
+++ dist/unplatform/curriculum/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import loader
+from django.http import HttpResponse
+import os
+from unplatform.settings import MODULES_LIST
+
+# Create your views here.
+
+def table_of_contents(request):
+    modules = os.listdir(MODULES_LIST)
+    # module_dictionary = dict(zip(modules, modules))
+    # for x in module_dictionary:
+    #     print (x, ":", module_dictionary[x])
+
+    template = loader.get_template('curriculum/index.html')
+    return HttpResponse(template.render({'modules':modules}))
