c3f692814b0b60fabb55ffd70ac8085f34d91f6a
Brandon <bhanks@mit.edu>
2016-04-07 08:39:12 -0400

    removed env files

--- unenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/brandon/versioned_projects/clix/unplatform_project/unenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
