d3883cc2acceb5604f4d62c8638bff53f0f75f44
Brandon <bhanks@mit.edu>
2016-04-07 17:29:52 -0400

    added win build script

---
+++ build_script_win32.bat
@@ -0,0 +1,16 @@
+:start
+del unenv
+del build
+del *.spec
+
+git pull
+
+virtualenv -p python3 unenv
+unenv/Scripts/activate
+pip install -r requirements.txt
+
+pyinstaller unserver.py --clean --distpath ../unplatform-distributable -n unplatform_win32 -y
+
+PING 1.1.1.1 -n 1 -w 60000 >NUL
+
+goto start
