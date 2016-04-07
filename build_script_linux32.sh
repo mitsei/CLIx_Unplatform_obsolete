d31b5320705d82fffbd4f9cb0ee8e4ac850c2082
Brandon <bhanks@mit.edu>
2016-04-07 12:17:42 -0400

    build script tweaking

--- build_script_linux32.sh
+++ build_script_linux32.sh
@@ -12,7 +12,7 @@ until [  $COUNTER -lt 10 ]; do
     source unenv/bin/activate
     pip install -r requirements.txt
     
-    cp unenv/lib/python3.4/site-packages/PyInstaller/bootloader/Linux-32bit unenv/lib/python3.4/site-packages/PyInstaller/bootloader/Linux-32bit-arm
+    cp -r unenv/lib/python3.4/site-packages/PyInstaller/bootloader/Linux-32bit unenv/lib/python3.4/site-packages/PyInstaller/bootloader/Linux-32bit-arm
     
     pyinstaller unserver.py --clean --distpath ../unplatform-distributable -n unplatform_linux64 -y
     
