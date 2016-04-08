4f47010ac4829b539b5028f0f7a1c613ab55792f
Brandon <bhanks@mit.edu>
2016-04-08 17:11:13 -0400

    added osx build script

---
+++ build_script_osx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+
+    rm -r build
+    rm *.spec
+    
+    git pull
+
+    pip install -r requirements.txt
+    
+    pyinstaller unserver.py --clean --distpath ../unplatform-distributable -n unplatform_osx -y
+    
+    
+    echo WAITING
+    
+    sleep 1h
+done
