a11b1912180eb267f8d551a72e64564b203562ac
Brandon <bhanks@mit.edu>
2016-04-11 16:01:40 -0400

    separated the tool api return into dirs & files

--- curriculum/views.py
+++ curriculum/views.py
@@ -1,36 +1,22 @@
 from django.shortcuts import loader
 from django.http import HttpResponse
-import os
 from unplatform.settings import MODULES_FOLDER, BASE_DIR
-
-# Create your views here.
-
-def table_of_contents(request):
-    modules = os.listdir(MODULES_FOLDER)
-    modules = sorted(modules)
-    # module_dictionary = dict(zip(modules, modules))
-    # for x in module_dictionary:
-    #     print (x, ":", module_dictionary[x])
-
-    template = loader.get_template('curriculum/index.html')
-    return HttpResponse(template.render({'modules':modules}))
-
-
-# def list_files(request):
-#
-#
-#     return HttpResponse(request)
-
 
 import os
 import posixpath
-
-from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.http import Http404
 from django.utils.six.moves.urllib.parse import unquote
 from django.views import static
 import json
+
+def table_of_contents(request):
+    modules = os.listdir(MODULES_FOLDER)
+    modules = sorted(modules)
+    template = loader.get_template('curriculum/index.html')
+    return HttpResponse(template.render({'modules':modules}))
+
+
 
 # Modified version of django.contrib.staticfiles.views which returns a directory listing as json
 def serve_module(request, path, insecure=False, **kwargs):
@@ -38,10 +24,16 @@ def serve_module(request, path, insecure=False, **kwargs):
     file_path =  os.path.join(BASE_DIR, 'common/', path)
     valid_path = os.path.isdir(file_path) and path != ''
     if valid_path:
-        file_list = os.listdir(file_path)
-        # file_string = ', '.join(map(str, file_list))
-        # files = '{"files":[' + file_string + ']}'
-        files = json.dumps({'files': file_list})
+        files = []
+        dirs = []
+        for f in os.listdir(file_path):
+            if os.path.isfile(os.path.join(file_path, f)):
+                files.append(f)
+            elif os.path.isdir(os.path.join(file_path, f)):
+                dirs.append(f)
+        files = sorted(files)
+        dirs = sorted(dirs)
+        files = json.dumps({'files': files, 'dirs': dirs})
 
     normalized_path = posixpath.normpath(unquote(path)).lstrip('/')
     absolute_path = finders.find(normalized_path)
