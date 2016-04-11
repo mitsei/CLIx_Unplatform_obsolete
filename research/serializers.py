e37a347101f2deaafdef18a90b42f6a52de5efb8
Brandon <bhanks@mit.edu>
2016-04-11 17:06:20 -0400

    forgot the serializer for language but it is in now

--- research/serializers.py
+++ research/serializers.py
@@ -7,7 +7,7 @@ from research.models import Fingerprint
 class FingerprintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fingerprint
-        fields = ('url', 'uuid', 'user_agent', 'screen_size', 'browser_url', 'client_ip', 'client_ip_other', 'server_ip', 'creation_time')
+        fields = ('url', 'uuid', 'user_agent', 'screen_size', 'browser_url', 'languages', 'client_ip', 'client_ip_other', 'server_ip', 'creation_time')
 
 #
 # class ModuleSerializer(serializers.ModelSerializer):
