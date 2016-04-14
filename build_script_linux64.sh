5cd402d50415203e9433be6ac6f7ddca8445ea81
Brandon <bhanks@mit.edu>
2016-04-13 20:20:26 -0400

    tweak

--- build_script_linux64.sh
+++ build_script_linux64.sh
@@ -9,15 +9,11 @@ until [  $COUNTER -lt 10 ]; do
     find . -name ".DS_Store" -type f -delete
 
     git pull
-
-    virtualenv -p python3 unenv
-    source unenv/bin/activate
     pip install -r requirements.txt
     
     pyinstaller unserver.py --clean --distpath ../unplatform-distributable -n unplatform_linux64 -y
     
     
     echo COUNTER $COUNTER
-    let COUNTER-=1
     sleep 1h
 done
