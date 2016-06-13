f641bffbb65d3b75be6659e004cab753349b5b0e
Brandon <bhanks@mit.edu>
2016-06-13 11:36:15 -0400

    fixing heroku

--- research/models.py
+++ research/models.py
@@ -10,7 +10,7 @@ class Configuration(models.Model):
 class UUID(models.Model):
     session_id = models.CharField(max_length=36, primary_key=True)
     creation_time = models.DateTimeField(default=timezone.now)
-    configuration = models.ForeignKey(Configuration, related_name='configuration', default='NOTSET')
+    # configuration = models.ForeignKey(Configuration, related_name='configuration', default='NOTSET')
     def __str__(self):
         return self.session_id
 # default should be Configuration.objects.latest('creation_time').school_id
@@ -25,7 +25,7 @@ class Fingerprint(models.Model):
     session_id = models.ForeignKey(UUID, related_name='fingerprints')
     user_agent = models.CharField(max_length=200) # not sure what a good length is yet
     screen_size = models.CharField(max_length=12, null=True)
-    browser_url = models.CharField(max_length=200, null=True) # also not sure about this length
+    browser_url = models.CharField(max_length=200, null=True) # also not suere about this length
     languages = models.CharField(max_length=50, null=True) # or this one, for that matter
     client_ip = models.CharField(max_length=15, null=True)
     client_ip_other = models.CharField(max_length=15, null=True)
