b228bbe477c957a67c44224a17c989b44810c2b3
Brandon <bhanks@mit.edu>
2016-06-15 18:09:13 -0400

    unproxy fix

--- unproxy.py
+++ unproxy.py
@@ -4,14 +4,14 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CERTFILE = os.path.join(BASE_DIR, "unplatform.cert.dummy.pem")
-KEYFILE = os.path.join(BASE_DIR, "unplatform.cert.dummy.pem")
+KEYFILE = os.path.join(BASE_DIR, "unplatform.key.dummy.pem")
 
 container = wsgi.WSGIContainer(application)
-http_server = httpserver.HTTPServer(container)
-# ,
-#                                     ssl_options = {
-#             "certfile":  CERTFILE,
-#                 "keyfile": KEYFILE }
+http_server = httpserver.HTTPServer(container,
+                                    ssl_options = {
+            "certfile":  CERTFILE,
+                "keyfile": KEYFILE })
+
 http_server.listen(8888)
 
 ioloop.IOLoop.current().start()
