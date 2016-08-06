9fe26e7e4b560b887b424dc4a7535d3703b61bde
Brandon <bhanks@mit.edu>
2016-08-05 22:07:24 -0400

    updated math game, added new assets

---
+++ unplatform_source/common/unplatform/settings.js
@@ -0,0 +1,15 @@
+window.DEFAULT_SETTINGS = {
+        user_id      : 0,
+        max_attempts : 1,
+        eid          : "external_identifier",
+        api_url      : "https://qbank-clix-dev.mit.edu/api/v1/",
+        bank:          "assessment.Bank%3A57867b0dc89cd93f9e22cc2f%40ODL.MIT.EDU%40ODL.MIT.EDU",
+        assessment_offered_id: "assessment.AssessmentOffered%3A5791382bc89cd96c58adf4cc%40ODL.MIT.EDU",
+        assessment_kind: "SUMMATIVE",
+        questions_per_section:1,
+        questions_per_page:1,
+        unlock_next: "ON_CORRECT",
+        locale: "en",
+        audio_recorder_timeout: 100
+      };
+window.DEFAULT_JWT = null;
