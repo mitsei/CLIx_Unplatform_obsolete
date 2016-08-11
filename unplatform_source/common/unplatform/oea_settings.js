701aec5ff6ce9c009c4670788efed68565ab757f
Brandon <bhanks@mit.edu>
2016-08-11 13:37:42 -0400

    added ability to getcookie to oea_settings

--- unplatform_source/common/unplatform/oea_settings.js
+++ unplatform_source/common/unplatform/oea_settings.js
@@ -1,15 +1,31 @@
-window.DEFAULT_SETTINGS = {
-        user_id      : 0,
-        max_attempts : 1,
-        eid          : "external_identifier",
-        api_url      : "https://qbank-clix-dev.mit.edu/api/v1/",
-        bank:          "assessment.Bank%3A57867b0dc89cd93f9e22cc2f%40ODL.MIT.EDU%40ODL.MIT.EDU",
-        assessment_offered_id: "assessment.AssessmentOffered%3A5791382bc89cd96c58adf4cc%40ODL.MIT.EDU",
-        assessment_kind: "SUMMATIVE",
-        questions_per_section:1,
-        questions_per_page:1,
-        unlock_next: "ON_CORRECT",
-        locale: "en",
-        audio_recorder_timeout: 100
-      };
-window.DEFAULT_JWT = null;
+(function() {
+
+// From http://www.w3schools.com/js/js_cookies.asp
+
+        function getCookie(cname) {
+                var name = cname + "=";
+                var ca = document.cookie.split(';');
+                for (var i = 0; i < ca.length; i++) {
+                        var c = ca[i];
+                        while (c.charAt(0) == ' ') c = c.substring(1);
+                        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+                }
+                return "";
+        }
+
+        window.DEFAULT_SETTINGS = {
+                user_id: 0,
+                max_attempts: 1,
+                eid: getCookie("session_uuid"),
+                api_url: "https://qbank-clix-dev.mit.edu/api/v1/",
+                bank: "assessment.Bank%3A57867b0dc89cd93f9e22cc2f%40ODL.MIT.EDU%40ODL.MIT.EDU",
+                assessment_offered_id: "assessment.AssessmentOffered%3A5791382bc89cd96c58adf4cc%40ODL.MIT.EDU",
+                assessment_kind: "SUMMATIVE",
+                questions_per_section: 1,
+                questions_per_page: 1,
+                unlock_next: "ON_CORRECT",
+                locale: "en",
+                audio_recorder_timeout: 100
+        };
+        window.DEFAULT_JWT = null;
+})();
