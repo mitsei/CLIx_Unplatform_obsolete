fa4665c638a435a7203f0e9937831512c8060487
Brandon <bhanks@mit.edu>
2016-08-11 19:55:12 -0400

    rewrote the build scripts to be easier to use, updated settings for standalone build, cleared out old distributables, ready to build

---
+++ build_scripts/build_script_linux64_ssl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd ..
+
+rm -r build
+rm *.spec
+find . -name "*.pyc" -type f -delete
+find . -name ".DS_Store" -type f -delete
+find . -name ".directory" -type f -delete
+
+git pull
+
+pip install -r unplatform_source/requirements.txt
+
+rm -r unplatform_distributable/unplatform_linux64
+
+pyinstaller unplatform_source/unserver_ssl.py --clean --distpath unplatform_distributable -n unplatform_linux64 -y
+pyinstaller unplatform_source/unworker.py --clean --distpath unplatform_distributable -n unplatform_linux64_worker -y
+
+rm -r build
+rm *.spec
+    
+rsync --remove-source-files -vr unplatform_distributable/unplatform_linux64_worker/ unplatform_distributable/unplatform_linux64/
+rm -r unplatform_distributable/unplatform_linux64_worker
+
+mv unplatform_distributable/unplatform_linux64/* unplatform_distributable/unplatform_linux64/unplatform/
+
+cp -r -v unplatform_source/common unplatform_distributable/unplatform_linux64/unplatform/
+cp -r -v unplatform_source/curriculum unplatform_distributable/unplatform_linux64/unplatform/
+cp -r -v unplatform_source/modules unplatform_distributable/unplatform_linux64/unplatform/
+cp -r -v unplatform_source/research unplatform_distributable/unplatform_linux64/unplatform/
+cp -r -v unplatform_source/unplatform unplatform_distributable/unplatform_linux64/unplatform/
+
+cp unplatform_source/unplatform.cert.dummy.pem unplatform_distributable/unplatform_linux64/unplatform/
+cp unplatform_source/unplatform.key.dummy.pem unplatform_distributable/unplatform_linux64/unplatform/
+
+cp unplatform_distributable/launchers/unplatform_linux64_ssl.sh unplatform_distributable/unplatform_linux64
+
+cp -r -v unplatform_distributable/readme unplatform_distributable/unplatform_linux64/readme
---
+++ build_scripts/build_script_win32_ssl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd ..
+
+rm -r build
+rm *.spec
+find . -name "*.pyc" -type f -delete
+find . -name ".DS_Store" -type f -delete
+find . -name ".directory" -type f -delete
+
+git pull
+
+pip install -r unplatform_source/requirements.txt
+
+rm -r unplatform_distributable/unplatform_win32
+
+pyinstaller unplatform_source/unserver_ssl.py --clean --distpath unplatform_distributable -n unplatform_win32 -y
+pyinstaller unplatform_source/unworker.py --clean --distpath unplatform_distributable -n unplatform_win32_worker -y
+
+rm -r build
+rm *.spec
+    
+rsync --remove-source-files -vr unplatform_distributable/unplatform_win32_worker/ unplatform_distributable/unplatform_win32/
+rm -r unplatform_distributable/unplatform_win32_worker
+
+mv unplatform_distributable/unplatform_win32/* unplatform_distributable/unplatform_win32/unplatform/
+
+cp -r -v unplatform_source/common unplatform_distributable/unplatform_win32/unplatform/
+cp -r -v unplatform_source/curriculum unplatform_distributable/unplatform_win32/unplatform/
+cp -r -v unplatform_source/modules unplatform_distributable/unplatform_win32/unplatform/
+cp -r -v unplatform_source/research unplatform_distributable/unplatform_win32/unplatform/
+cp -r -v unplatform_source/unplatform unplatform_distributable/unplatform_win32/unplatform/
+
+cp unplatform_source/unplatform.cert.dummy.pem unplatform_distributable/unplatform_win32/unplatform/
+cp unplatform_source/unplatform.key.dummy.pem unplatform_distributable/unplatform_win32/unplatform/
+
+cp unplatform_distributable/launchers/unplatform_win32_ssl.sh unplatform_distributable/unplatform_win32
+
+cp -r -v unplatform_distributable/readme unplatform_distributable/unplatform_win32/readme
