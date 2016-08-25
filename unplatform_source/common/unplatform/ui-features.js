f71c68883e2e9400057672f450b61827aac1e93f
Brandon <bhanks@mit.edu>
2016-08-25 16:34:31 -0400

    integrated new styling, added a new epub

--- unplatform_source/common/unplatform/ui-features.js
+++ unplatform_source/common/unplatform/ui-features.js
@@ -4,6 +4,7 @@ $(function() {
 
   $('.but-select').on('click', function(e) {
     $(this).children('button.selected').removeClass('selected');
+    $('.count-select').children('button.selected-count').removeClass('selected-count');
     var targetEl = $(e.target).closest('button');
     targetEl.addClass('selected');
   });
