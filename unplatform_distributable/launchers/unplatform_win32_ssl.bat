88866036adf01249340234dbfbe7c734375f62a4
Brandon <bhanks@mit.edu>
2016-09-12 13:54:10 -0400

    updated OEA, updated qbank, removed some old unused directories

--- unplatform_distributable/launchers/unplatform_win32_ssl.bat
+++ unplatform_distributable/launchers/unplatform_win32_ssl.bat
@@ -3,5 +3,5 @@
 start /MIN "unplatform" unplatform/unplatform_win32.exe
 ping localhost 
 start /MIN "unworker" unplatform/unplatform_win32_worker.exe
-start /MIN "qbank" qbank-lite-v0.0.65-ssl.exe
+start /MIN "qbank" qbank-lite-v0.0.76-32bit-ssl.exe
 start chrome https://localhost:8888
