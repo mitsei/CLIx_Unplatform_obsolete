8b909b97c0f5330ef03cc3cd27ae3ba7a45b7721
Brandon <bhanks@mit.edu>
2016-09-21 11:03:32 -0400

    possible bug fix to reporting function in iframes

--- unplatform_source/common/unplatform/unplatform.js
+++ unplatform_source/common/unplatform/unplatform.js
@@ -108,8 +108,11 @@ function callback(e) {
 
 (function(){if (document.addEventListener) {
 		document.addEventListener('click', callback, false);
+		document.getElementsByTagName('iframe')[0].contentWindow.document.addEventListener('click', callback, false);
 	} else {
-		document.attachEvent('onclick', callback);}
+		document.attachEvent('onclick', callback);
+		document.getElementsByTagName('iframe')[0].contentWindow.document.attachEvent('onclick', callback);
+	}
 })();
 
 
