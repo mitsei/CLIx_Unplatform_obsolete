630c2e4f141184a042497df21918cc56e3de765a
cjshaw <cjshaw@mit.edu>
2016-09-29 10:32:13 -0400

    update windows launcher to take the highest version qbank in the directory

--- unplatform_distributable/launchers/unplatform_win32_ssl.bat
+++ unplatform_distributable/launchers/unplatform_win32_ssl.bat
@@ -3,5 +3,7 @@
 start /MIN "unplatform" unplatform/unplatform_win32.exe
 ping localhost
 start /MIN "unworker" unplatform/unplatform_win32_worker.exe
-start /MIN "qbank" qbank-lite-v1.0.6-32bit-ssl.exe
+
+FOR /F "delims=|" %%I IN ('DIR "qbank-lite*.exe" /B /O:N') DO SET NewestQBank=%%I
+start /MIN "qbank" "%NewestQBank%"
 start chrome https://localhost:8888
