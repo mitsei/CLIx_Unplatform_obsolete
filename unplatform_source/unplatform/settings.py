e36baf3dfb295167808f501913ffbce653b02c95
cjshaw <cjshaw@mit.edu>
2016-10-03 09:04:14 -0400

    use django settings for STATIC_URL and MEDIA_URL in templates

--- unplatform_source/unplatform/settings.py
+++ unplatform_source/unplatform/settings.py
@@ -193,7 +193,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/modules/'
+STATIC_URL = '/static'
+MEDIA_URL = '/media'
 STATIC_ROOT = 'unused'
 
 STATICFILES_DIRS = (
