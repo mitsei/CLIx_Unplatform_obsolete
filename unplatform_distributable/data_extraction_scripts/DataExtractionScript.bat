ea944242a1c31c2f0bd28c8f36fd02fc3936e5fd
cjshaw <cjshaw@mit.edu>
2016-10-04 09:37:31 -0400

    fix data extraction script to copy AssessmentSection instead of AssessmentOffered

--- unplatform_distributable/data_extraction_scripts/DataExtractionScript.bat
+++ unplatform_distributable/data_extraction_scripts/DataExtractionScript.bat
@@ -24,7 +24,7 @@ echo Creating temporary directory called "%Dirname%"
 echo ===========================================================================
 md "%Dirname%"
 md "%Dirname%\assessment"
-md "%Dirname%\assessment\AssessmentOffered"
+md "%Dirname%\assessment\AssessmentSection"
 md "%Dirname%\assessment\AssessmentTaken"
 md "%Dirname%\logging"
 md "%Dirname%\logging\Log"
@@ -40,7 +40,7 @@ echo ===========================================================================
 echo Coping data from assessment folders
 echo ===========================================================================
 copy "webapps\CLIx\datastore\assessment" "%Dirname%\Assessment"
-copy "webapps\CLIx\datastore\assessment\AssessmentOffered" "%Dirname%\Assessment\AssessmentOffered"
+copy "webapps\CLIx\datastore\assessment\AssessmentSection" "%Dirname%\Assessment\AssessmentSection"
 copy "webapps\CLIx\datastore\assessment\AssessmentTaken" "%Dirname%\Assessment\AssessmentTaken"
 
 echo ===========================================================================
