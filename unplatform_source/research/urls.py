a323ae55c806d103f62ba4b8c0dd69ba10a2e45d
cjshaw <cjshaw@mit.edu>
2016-10-07 10:31:31 -0400

    fix url routing

--- unplatform_source/research/urls.py
+++ unplatform_source/research/urls.py
@@ -6,12 +6,13 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'uuids', views.UUIDViewSet)
 router.register(r'fingerprints', views.FingerprintViewSet)
-router.register(r'appdata', views.AppDataViewSet)
+# router.register(r'appdata', views.AppDataViewSet)
 router.register(r'configuration', views.ConfigurationViewSet)
 router.register(r'users', views.UserViewSet)
 
 
 urlpatterns = [
+    url(r'^api/appdata/?$', views.AppDataViewSet.as_view()),
     url(r'^api/', include(router.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     ]
